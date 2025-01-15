20f5a0e2f552c42b77d65a0a67cc6e4c3f006661
Giacomo Righetti <righetti.giacomo@live.com>
2025-01-15 05:59:23 +0000

    Fixed 'should be defined' unit test

--- apps/api/src/task/task.service.spec.ts
+++ apps/api/src/task/task.service.spec.ts
@@ -1,21 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskService } from './task.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './entities/task.entity';
+import { Product } from '../product/entities/product.entity';
 
 describe('TaskService', () => {
-  let service: TaskService;
+    let service: TaskService;
+    let taskRepository: Repository<Task>;
+    let productRepository: Repository<Product>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TaskService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                TaskService,
+                {
+                    provide: getRepositoryToken(Task),
+                    useClass: Repository, // Mock Repository for Task
+                },
+                {
+                    provide: getRepositoryToken(Product),
+                    useClass: Repository, // Mock Repository for Product
+                },
+            ],
+        }).compile();
 
-    service = module.get<TaskService>(TaskService);
-  });
+        service = module.get<TaskService>(TaskService);
+        taskRepository = module.get<Repository<Task>>(getRepositoryToken(Task));
+        productRepository = module.get<Repository<Product>>(getRepositoryToken(Product));
+    });
 
-  it('should be defined', () => {
-    // @fixme basic test is failing
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 
-  // @fixme add tests for service crud actions
+    // @fixme add tests for service crud actions
 });

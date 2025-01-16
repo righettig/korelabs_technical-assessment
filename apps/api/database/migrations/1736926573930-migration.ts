3d59bc6ac67dc3e2bc9e5e2d962a3869a18c721b
Giacomo Righetti <righetti.giacomo@live.com>
2025-01-16 14:16:21 +0000

    Bugfix

--- apps/api/database/migrations/1736926573930-migration.ts
+++ apps/api/database/migrations/1736926573930-migration.ts
@@ -5,7 +5,7 @@ export class Migration1720026827434 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "tasks" DROP CONSTRAINT "FK_18a04c9720ea052eac80971ba48"`);
-        await queryRunner.query(`ALTER TABLE "tasks" ADD CONSTRAINT "FK_18a04c9720ea052eac80971ba48" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE CASCADE ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "tasks" ADD CONSTRAINT "FK_18a04c9720ea052eac80971ba48" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {

c8a73b3571f238719e50fcb50116c017be88934c
Giacomo Righetti <righetti.giacomo@live.com>
2025-01-16 13:07:20 +0000

    Fixed unit test

--- apps/web/src/app/components/layout/layout.component.spec.ts
+++ apps/web/src/app/components/layout/layout.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LayoutComponent } from './layout.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
@@ -8,7 +10,15 @@ describe('LayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LayoutComponent]
+      imports: [LayoutComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of([{id: 1}]),
+          },
+        },
+      ]
     })
     .compileComponents();
 

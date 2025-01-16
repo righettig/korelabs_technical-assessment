c988d018c7bd70737d1d6be8b60fadc0e7291903
Giacomo Righetti <righetti.giacomo@live.com>
2025-01-16 13:03:49 +0000

    Fixed unit tests

--- apps/web/src/app/components/products/products.component.spec.ts
+++ apps/web/src/app/components/products/products.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
+import { Router } from '@angular/router';
+import { ProductsService } from '../../services/products.service';
+import { of } from 'rxjs';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
-
+  let mockProductsService: jasmine.SpyObj<ProductsService>;
+  let mockRouter: jasmine.SpyObj<Router>;
+  
   beforeEach(async () => {
+    mockProductsService = jasmine.createSpyObj('ProductsService', ['findAll', 'delete']);
+    mockProductsService.findAll.and.returnValue(of([])); // Mock findAll method to return an empty observable
+    mockRouter = jasmine.createSpyObj('Router', ['navigate']); // Mock navigate method
+
     await TestBed.configureTestingModule({
-      imports: [ProductsComponent]
+      imports: [ProductsComponent],
+      providers: [
+        { provide: ProductsService, useValue: mockProductsService },
+        { provide: Router, useValue: mockRouter }
+      ]
     })
     .compileComponents();
 
